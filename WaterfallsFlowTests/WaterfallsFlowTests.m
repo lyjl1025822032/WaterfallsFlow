b4e48fb3adcffb83632f9d396f747b654505d624
lyjl1025822032 <m18272733524@163.com>
2017-06-22 09:18:09 +0800

    Initial Commit

---
+++ WaterfallsFlowTests/WaterfallsFlowTests.m
@@ -0,0 +1,39 @@
+//
+//  WaterfallsFlowTests.m
+//  WaterfallsFlowTests
+//
+//  Created by yao on 2017/6/22.
+//  Copyright © 2017年 yao. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WaterfallsFlowTests : XCTestCase
+
+@end
+
+@implementation WaterfallsFlowTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
